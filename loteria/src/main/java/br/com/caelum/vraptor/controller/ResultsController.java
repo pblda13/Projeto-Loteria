c10e62a4183407198c40641a1797d2b79d99c6f2
Pâmela Borges <pamela_borges18@hotmail.com>
2023-06-29 13:31:32 -0300

    first commit

---
+++ loteria/src/main/java/br/com/caelum/vraptor/controller/ResultsController.java
@@ -0,0 +1,15 @@
+package br.com.caelum.vraptor.controller;
+
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Path;
+
+@Path("results")
+@Controller
+public class ResultsController { //(Nome da Pasta)
+    
+    @Get("")
+    public void results(){ //(Nome do Arquivo)
+    
+    }
+}
---
+++ loteria/src/main/java/br/com/caelum/vraptor/dao/UsuarioDAO.java
@@ -0,0 +1,19 @@
+package br.com.caelum.vraptor.dao;
+
+import br.com.caelum.vraptor.model.UsuarioModel;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+@RequestScoped
+public class UsuarioDAO extends DAO<UsuarioModel>{
+
+    @Inject
+    public UsuarioDAO(EntityManager em) {
+        super(em, UsuarioModel.class);
+    }
+    
+    @Deprecated public UsuarioDAO() { this(null); }
+    
+    
+}
